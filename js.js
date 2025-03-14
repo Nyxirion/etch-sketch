42479d332565a6aba75465820ce8b7b1efe9621a
MiSa0629 <megasy77@gmail.com>
2025-03-14 08:48:35 -0400

    Finish the etch-skectch

--- js.js
+++ js.js
@@ -1,22 +1,23 @@
 const containerSize = 500;
-let maxCell = 16;
 const container = document.querySelector(".container");
 
-for(let i = 0; i < maxCell * maxCell; i++){
-    const div = document.createElement("div");
-    div.classList.add("grid-cell");
-    div.style.width = (containerSize/maxCell) + "px";
-    div.style.height = (containerSize/maxCell) + "px";
-    container.appendChild(div);
+function createGrid(cell = 16){
+    maxCell = cell;
+    for(let i = 0; i < maxCell * maxCell; i++){
+        const div = document.createElement("div");
+        div.classList.add("grid-cell");
+        div.style.width = (containerSize/maxCell) + "px";
+        div.style.height = (containerSize/maxCell) + "px";
+        container.appendChild(div);
+    }
 }
 
-    container.addEventListener("mouseover", (event) => {
-        let target = event.target;
-        if (target.getAttribute("class") == "grid-cell"){
-            target.style.backgroundColor = "lightblue";
-        }
-    });
-}
+container.addEventListener("mouseover", (event) => {
+    let target = event.target;
+    if (target.getAttribute("class") == "grid-cell"){
+        target.style.backgroundColor = "lightblue";
+    }
+});
 
 function deleteGrid(){
     while (container.firstChild) {
